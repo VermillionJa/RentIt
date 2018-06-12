3d81de2ccd28a29c945528743262c58d50e10b46
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-11 19:40:38 -0600

    Added the Movie and MovieGenre Entities Added a Movie Rating enum

---
+++ RentIt/RentIt/Data/Entities/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.Data.Entities
+{
+    /// <summary>
+    /// Represents a movie
+    /// </summary>
+    public class Movie
+    {
+        /// <summary>
+        /// The Id of the movie
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The title of the movie
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The description of the movie
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The date the movie was released
+        /// </summary>
+        public DateTime ReleaseDate { get; set; }
+
+        /// <summary>
+        /// The rating of the movie provided by the MPAA
+        /// </summary>
+        public MovieRating Rating { get; set; }
+
+        /// <summary>
+        /// The genre of the movie
+        /// </summary>
+        public MovieGenre Genre { get; set; }
+    }
+}
---
+++ RentIt/RentIt/Data/Entities/MovieGenre.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.Data.Entities
+{
+    /// <summary>
+    /// Represents a genre of movies
+    /// </summary>
+    public class MovieGenre
+    {
+        /// <summary>
+        /// The Id of the genre
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the genre
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
