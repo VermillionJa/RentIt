e60d70c250644eabff99f7dd27034b356603dfbb
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-11 20:14:12 -0600

    Added Rental Transaction and Rental Transaction Entities

---
+++ RentIt/RentIt/Data/Entities/RentalTransaction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.Data.Entities
+{
+    /// <summary>
+    /// Represents a movie rental transaction
+    /// </summary>
+    public class RentalTransaction
+    {
+        /// <summary>
+        /// The Id of the Record
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The customer the movies were rented to
+        /// </summary>
+        public Customer Customer { get; set; }
+
+        /// <summary>
+        /// The date the movies were rented on
+        /// </summary>
+        public DateTime TransactionDate { get; set; }
+
+        /// <summary>
+        /// A collection of movies rented
+        /// </summary>
+        public List<RentalTransactionItems> Items { get; set; }
+
+        /// <summary>
+        /// The total Late Fees paid in this transaction
+        /// </summary>
+        public decimal LateFeesCharged { get; set; }
+
+        /// <summary>
+        /// The total Rental Fees paid in this transaction
+        /// </summary>
+        public decimal RentalFeesCharged { get; set; }
+
+        /// <summary>
+        /// The total amount paid in this transaction
+        /// </summary>
+        public decimal TotalAmountCharged { get; set; }
+    }
+}
