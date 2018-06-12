63a6d5482a7b9b21c902405606bced59d3b6025a
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-11 19:57:25 -0600

    Added the Customer Entity

---
+++ RentIt/RentIt/Data/Entities/Customer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.Data.Entities
+{
+    /// <summary>
+    /// Represents information regarding a customer who rented a movie
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// The Id of the customer
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The first name of the customer
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name of the customer
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The phone number of the customer
+        /// </summary>
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// The street address of the customer
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// The additional street address of the customer
+        /// </summary>
+        public string Address2 { get; set; }
+
+        /// <summary>
+        /// The city the customer lives in
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// The state the customer lives in
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// The zip code the customer lives in
+        /// </summary>
+        public string Zip { get; set; }
+    }
+}
