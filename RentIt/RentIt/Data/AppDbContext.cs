22bfb8b6d6df76f65fdb9c025ba062d1c3f006dc
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-12 20:13:17 -0600

    Added entities to the AppDbContext

--- RentIt/RentIt/Data/AppDbContext.cs
+++ RentIt/RentIt/Data/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RentIt.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,31 @@ namespace RentIt.Data
     /// </summary>
     public class AppDbContext : DbContext
     {
+        /// <summary>
+        /// The Customers stored in the database
+        /// </summary>
+        public DbSet<Customer> Customers { get; set; }
+
+        /// <summary>
+        /// The Inventory Items stored in the database
+        /// </summary>
+        public DbSet<InventoryItem> InventoryItems { get; set; }
+
+        /// <summary>
+        /// The Movies stored in the database
+        /// </summary>
+        public DbSet<Movie> Movies { get; set; }
+
+        /// <summary>
+        /// The Movie Genres stored in the database
+        /// </summary>
+        public DbSet<MovieGenre> MovieGenres { get; set; }
+
+        /// <summary>
+        /// The Rental Transactions stored in the database
+        /// </summary>
+        public DbSet<RentalTransaction> RentalTransactions { get; set; }
+
         /// <summary>
         /// Initializes a new instance of the AppDbContext class configured using the given options
         /// </summary>
