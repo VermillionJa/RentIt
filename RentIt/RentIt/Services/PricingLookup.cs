88f287221100562767848335e1d002583418b8cf
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-23 16:40:25 -0600

    Added a GetRentalPricing method

--- RentIt/RentIt/Services/PricingLookup.cs
+++ RentIt/RentIt/Services/PricingLookup.cs
@@ -11,7 +11,16 @@ namespace RentIt.Services
     /// </summary>
     public class PricingLookup
     {
+        private const int DaysInYear = 365;
+
+        private enum MoviePricingCategory { New, Regular, Old };
+        
         private readonly IConfiguration _config;
+
+        private readonly double _newCategoryLimitYears;
+        private readonly double _regularCategoryLimitYears;
+
+        private readonly Dictionary<MoviePricingCategory, decimal> rentalPricing;
 
         /// <summary>
         /// Initializes a new instance of the PricingLookup class
@@ -20,8 +29,45 @@ namespace RentIt.Services
         public PricingLookup(IConfiguration config)
         {
             _config = config;
+
+            _newCategoryLimitYears = double.Parse(_config["Pricing:CategoryLimits:New"]);
+            _regularCategoryLimitYears = double.Parse(_config["Pricing:CategoryLimits:Regular"]);
+
+            rentalPricing = new Dictionary<MoviePricingCategory, decimal>
+            {
+                { MoviePricingCategory.New, decimal.Parse(_config["Pricing:Rentals:New"]) },
+                { MoviePricingCategory.Regular, decimal.Parse(_config["Pricing:Rentals:Regular"]) },
+                { MoviePricingCategory.Old, decimal.Parse(_config["Pricing:Rentals:Old"]) }
+            };
         }
 
+        public decimal GetRentalPricing(int numberOfDays, DateTime dateMovieReleased)
+        {
+            var pricingCategory = GetPricingCategory(dateMovieReleased);
+            var rentalPricePerDay = rentalPricing[pricingCategory];
 
+            return rentalPricePerDay * numberOfDays;
+        }
+
+        private MoviePricingCategory GetPricingCategory(DateTime dateMovieReleased)
+        {
+            var newCategoryLimit = TimeSpan.FromDays(DaysInYear * _newCategoryLimitYears);
+            var regularCategoryLimit = TimeSpan.FromDays(DaysInYear * _regularCategoryLimitYears);
+
+            var timeReleased = DateTime.Now.Date - dateMovieReleased.Date;
+
+            var pricingCategory = MoviePricingCategory.Old;
+
+            if (timeReleased <= newCategoryLimit)
+            {
+                pricingCategory = MoviePricingCategory.New;
+            }
+            else if (timeReleased <= regularCategoryLimit)
+            {
+                pricingCategory = MoviePricingCategory.Regular;
+            }
+
+            return pricingCategory;
+        }
     }
 }
