9f004ebef93063de7ba1dcecb9acb67e19944523
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-06-23 15:52:33 -0600

    Created the Pricing Lookup Service

---
+++ RentIt/RentIt/Services/PricingLookup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.Services
+{
+    /// <summary>
+    /// A Service for retrieving Pricing information for Rentals and Late Fees
+    /// </summary>
+    public class PricingLookup
+    {
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Initializes a new instance of the PricingLookup class
+        /// </summary>
+        /// <param name="config">The Site Configuration object injected via DI</param>
+        public PricingLookup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+    }
+}
