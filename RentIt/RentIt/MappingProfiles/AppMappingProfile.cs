b198fc1236e4f34fe3a0312555f6555ea4a50feb
Jake Vermillion <jacob.a.vermillion@gmail.com>
2018-07-01 13:52:59 -0600

    Added and configured AutoMapper

---
+++ RentIt/RentIt/MappingProfiles/AppMappingProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using RentIt.Data.Entities;
+using RentIt.Models.Movies;
+using RentIt.Services.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RentIt.MappingProfiles
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile(IMoviesRepo moviesRepo)
+        {
+            //Movie -> MovieDto
+            CreateMap<Movie, MovieDto>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
+
+            //Movie -> AddMovieDto
+            CreateMap<Movie, AddMovieDto>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
+
+            //AddMovieDto -> Movie
+            CreateMap<AddMovieDto, Movie>().ForMember(dest => dest.Genre, opt => opt.ResolveUsing(src => moviesRepo.GetGenreByName(src.Genre)));
+        }
+    }
+}
